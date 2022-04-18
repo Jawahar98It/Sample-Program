2daa085eec5b27adc6ec2acd01ed29154fd0d458
Jawahar98It <103891438+Jawahar98It@users.noreply.github.com>
2022-04-18 20:59:00 +0530

    This is use to test celsicus

---
+++ celsicus.java
@@ -0,0 +1,20 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class celsicus {
+
+	public static void main(String[] args) {
+		int celsicus,farenheit,kelvin;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value in Celcius: ");
+		celsicus = sc.nextInt();
+		farenheit = (celsicus*9/5)+32;
+		System.out.println(farenheit);
+		
+		
+
+
+	}
+
+}
