6529d192e3a649764bc7c80f04e16e7ae0deee51
Sathyapriyamca <98675170+Sathyapriyamca@users.noreply.github.com>
2022-05-15 12:26:59 +0530

    Update celsicus.java
    change celsius value

--- celsicus.java
+++ celsicus.java
@@ -7,7 +7,7 @@ public class celsicus {
 	public static void main(String[] args) {
 		int celsicus,farenheit,kelvin;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value in Celcius: ");
+		System.out.println("Enter the value in celcius: ");
 		celsicus = sc.nextInt();
 		farenheit = (celsicus*9/5)+32;
 		System.out.println(farenheit);
